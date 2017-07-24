4ae24eb2e9ddd49ef855d52fbfb8ed573c139f39
Ryno Mathee <rmathee@gmail.com>
2017-07-24 17:41:27 +0200

    Travis build desktop

--- src/js/controllers/preferencesAltCurrency.js
+++ src/js/controllers/preferencesAltCurrency.js
@@ -10,7 +10,7 @@ angular.module('copayApp.controllers').controller('preferencesAltCurrencyControl
       var unusedCurrencyList = [{
         isoCode: 'LTL'
       }, {
-        isoCode: 'BTC'
+        isoCode: 'PART'
       }];
       rateService.whenAvailable(function() {
 
