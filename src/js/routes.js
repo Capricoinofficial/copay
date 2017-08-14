99ca4f7b378145783de3d89ea9cfd918699da85b
rynomster <rmathee@gmail.com>
2017-08-14 22:34:47 +0200

    Broken routes

--- src/js/routes.js
+++ src/js/routes.js
@@ -1177,7 +1177,7 @@ angular.module('copayApp').config(function(historicLogProvider, $provide, $logPr
             templateUrl: 'views/preferencesBitpayServices.html'
           }
         }
-      });
+      });*/
   })
   .run(function($rootScope, $state, $location, $log, $timeout, startupService, ionicToast, fingerprintService, $ionicHistory, $ionicPlatform, $window, appConfigService, lodash, platformInfo, profileService, uxLanguage, gettextCatalog, openURLService, storageService, scannerService, configService, emailService, /* plugins START HERE => */ coinbaseService, glideraService, amazonService, bitpayCardService, applicationService, mercadoLibreService) {
 
